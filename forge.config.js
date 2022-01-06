9a3737ccb82e9ff93203dbcb9c01ecd14f1d7766
Bruce Hsu <bruce0205@gmail.com>
2022-01-06 09:03:17 +0800

    first commit

---
+++ forge.config.js
@@ -0,0 +1,46 @@
+const { utils: { fromBuildIdentifier } } = require('@electron-forge/core')
+
+module.exports = {
+  packagerConfig: {
+    executableName: "Folder Tagger",
+    name: "Folder Tagger",
+    appBundleId: fromBuildIdentifier({ beta: 'com.beta.app', prod: 'com.app' })
+  },
+  electronRebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "forge_dev",
+        exe: "forge_dev.exe",
+      }
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: [
+        "darwin"
+      ]
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {}
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {}
+    }
+  ],
+  "publisher": {
+    name: '@electron-forge/publisher-github',
+    config: {
+      repository: {
+        owner: 'me',
+        name: 'awesome-thing'
+      },
+      prerelease: true
+    }
+  },
+  plugins: [],
+  hooks: [],
+  buildIdentifier: process.env.IS_BETA ? 'beta' : 'prod'
+}
